b1ef047d3c37fcead8e726618a1b99cb2cca73b5
jhnlee817 <yobjohn2@yahoo.com>
2022-11-14 17:09:28 -0600

    get navbar working properly

--- script.js
+++ script.js
@@ -15,4 +15,29 @@ window.addEventListener("scroll", () => {
   lastScrollY = window.scrollY;
 });
 
+document.getElementById("menu").addEventListener("click", openMenu);
+
+// console.log(`menu: ${document.getElementById("menu")}`);
+
+function openMenu() {
+  document.getElementById("dropdown").classList.toggle("active");
 }
+
+console.log(openMenu);
+
+};
+
+// const menu = document.querySelector(".menu-btn");
+// const navToggle = document.querySelector(".nav__link");
+
+// navToggle.addEventListener("click", () => {
+//   const visibility = menu.getAttribute("data-visible");
+
+//   if (visibility === "false") {
+//     menu.setAttribute("data-visible", true);
+//   } else if (visibility === "true") {
+//     menu.setAttribute("data-visible", false)
+//   }
+
+// });
+
