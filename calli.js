5aba9fa69323fc90bc1441b688031452a3cf186d
jhnlee817 <yobjohn2@yahoo.com>
2022-12-01 15:31:21 -0600

    add modal for videos

---
+++ calli.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById("video-modal");
+var btn = document.getElementById("modal-button");
+var span = document.getElementsByClassName("modal-close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
