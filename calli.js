dc248ddaba7e5166a82af344162b43fbb38f6044
jhnlee817 <yobjohn2@yahoo.com>
2022-12-08 11:57:04 -0600

    fix responsiveness issue with video modal

--- calli.js
+++ calli.js
@@ -1,10 +1,11 @@
-var modal = document.getElementById("video-modal");
-var btn = document.getElementById("modal-button");
-var span = document.getElementsByClassName("modal-close")[0];
+var modal = document.querySelector(".popup-video");
+var btn = document.querySelector(".video");
+var span = document.querySelector(".close");
 
 btn.onclick = function() {
     modal.style.display = "block";
 }
+
 
 span.onclick = function() {
     modal.style.display = "none";
@@ -15,3 +16,22 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
+// var modal = document.getElementById("video-modal");
+// var btn = document.getElementById("modal-button");
+// var span = document.getElementsByClassName("modal-close")[0];
+
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
