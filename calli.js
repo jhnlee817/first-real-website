6ada31116c569ab84d9bc6be45b206525212e98f
jhnlee817 <yobjohn2@yahoo.com>
2022-12-12 12:57:03 -0600

    finish popup modals

--- calli.js
+++ calli.js
@@ -33,8 +33,33 @@ window.onclick = function(event) {
     if (event.target == modal2) {
         modal2.style.display = "none";
     }
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
+var modal3 = document.querySelector(".popup-video3");
+var btn3 = document.querySelector(".video3");
+var span3 = document.querySelector(".close3");
+
+btn3.onclick = function() {
+    modal3.style.display = "block";
+}
+span3.onclick = function() {
+    modal3.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
 
