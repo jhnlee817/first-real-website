a68e6be681ede7965148c2877e854b6779d0ed79
jhnlee817 <yobjohn2@yahoo.com>
2022-12-21 15:06:05 -0600

    clean up redundant code

--- calli.js
+++ calli.js
@@ -63,21 +63,3 @@ window.onclick = function(event) {
 
 
 
-
-// var modal = document.getElementById("video-modal");
-// var btn = document.getElementById("modal-button");
-// var span = document.getElementsByClassName("modal-close")[0];
-
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
-
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
